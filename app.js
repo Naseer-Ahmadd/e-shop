001ca1d063eaf2db331981ec8c50a727720db770
Naseer-Ahmadd <naseerrather58@gmail.com>
2022-02-03 21:54:14 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,218 @@
+var products =[
+
+    //books
+    {
+        P_name:"Wings of Fire",
+        p_cost: 189,
+        p_img:'imgs/admin.jpeg'
+
+    },
+    {
+        P_name:"Modern History",
+        p_cost: 349,
+
+    },
+    {
+        P_name:"Rich Dad Poor Dad",
+        p_cost: 180,
+
+    },
+    {
+        P_name:"To Kill A Mockingbird",
+        p_cost: 170,
+
+    },
+    //watches
+    //4
+    {
+        P_name:"David Miller DMRCM314 PU Analog Men's Watch",
+        p_cost: 259,
+
+    },
+    {
+        P_name:"David Miller DMRCM314 PU Analog Men's Watch",
+        p_cost: 349,
+
+    },
+    {
+        P_name:"Lois Caron LCS-8189 Stainless Steel Analog Men's Watch",
+        p_cost:339
+    },
+    {
+        P_name:"Lois Caron LCS-8190 Stainless Steel Analog Men's Watch",
+        p_cost:329
+    },
+    //mob
+    //8
+    {
+        P_name:"Mi 11X 5G Cosmic Black 6GB RAM 128GB",
+        p_cost:27999
+    },
+    {
+       P_name:"OnePlus Nord CE 5G (Blue Void, 8GB RAM, 128GB Storage)",
+       p_cost:24999
+    },
+    {
+       P_name:"OPPO A31 (Mystery Black, 6GB RAM, 128GB Storage)",
+       p_cost:12999
+    },
+    {
+       P_name:"OPPO A74 5G (Fantastic Purple,6GB RAM,128GB Storage)",
+       p_cost:16999
+    },
+    {
+       P_name:"realme narzo 50A (Oxygen Green , 4GB RAM + 64 GB Storage)",
+       p_cost:11499
+    },
+
+
+
+]
+// localStorage.clear()
+
+// var datas = JSON.parse(localStorage["mydatas"]);
+
+function addToCart(i){
+    alert("item added")
+    window.location.reload();
+   
+    var cart = new Array()
+    cart= JSON.parse(localStorage.getItem("cart"));
+    //console.log(cart);
+   
+    if(cart==null)
+    {
+        cart=new Array();
+        cart.push(products[i]);
+        
+    }else{
+        cart.push(products[i])
+
+    }
+    localStorage.setItem("cart", JSON.stringify(cart));
+    console.log(cart);
+
+
+}
+
+function getCart(){
+
+    // var cartItem = new Array()
+    cart = JSON.parse(localStorage.getItem("cart"))
+   console.log(cart);
+    // return "<h1>No Items</h1>"
+
+    if(cart == null){
+
+        return "<h1>No Items</h1>"
+        
+    }
+    else{
+        let html="";
+        for(let i=0;i<cart.length;i++)
+        {
+            // console.log(i);
+          html= html+ `
+          <div class="card" style="width: 48rem;">
+            <div class="card-body">
+                <h5 class="card-title ml-5">${cart[i].P_name}</h5>
+                <h6 class="card-subtitle mb-2 ml-5">${cart[i].p_cost}</h6>
+                
+                
+            </div>
+            
+          </div>       
+         `
+        }
+        return html;         
+    }
+}
+
+
+
+
+
+
+function total(){
+let cartTotal = 0;
+ let price = 0;
+//  JSON.parse(localStorage.getItem("cart"))
+   
+ cart = JSON.parse(localStorage.getItem("cart"))
+//    console.log(cart);
+    // return "<h1>No Items</h1>"
+
+    if(cart == null){
+
+        return 
+        
+    }
+      
+    for(let i=0;i<cart.length;i++)
+    {
+        // console.log(cart[i].p_cost);
+
+        cartTotal=cartTotal+cart[i].p_cost
+          
+     }
+    //  console.log(cartTotal);
+
+     return cartTotal;
+
+    }
+
+
+
+    
+function totalItems(){
+   
+       
+     cart = JSON.parse(localStorage.getItem("cart"))
+    
+    
+        if(cart == null){
+    
+            return 
+            
+        }
+          
+        return cart.length
+
+        
+    
+        }
+    
+      console.log(totalItems());  
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ==================LOGIN===========================
+function login(form)
+{
+ if(form.uname.value == "admin" && form.upwd.value == "admin")
+  {
+    alert("login successful")
+    window.open('index.html')
+  }
+ else
+ {
+   alert("Error Password or Username")
+  }
+}
---
+++ header.js
@@ -0,0 +1,112 @@
+class Myheader extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML=`
+        
+        <header > 
+        <nav class="navbar navbar-expand-lg fixed-top  navbar-light bg-light py-3"  >
+            <div class="container-fluid" >
+              
+                <div class="navbar-brand" style="cursor: pointer;margin-left:70px;">
+                <a class="navbar-brand" href="#" >E-Shop</a>
+                  
+                </div>
+    
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+    
+              <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.html" aria-current="page">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="mobiles.html" aria-current="page">Mobiles</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link "   href="books.html" aria-current="page" href="index.html" >Books</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link "  href="watches.html" aria-current="page"  >watches</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link"  >Laptops</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link "   href="#">cloths</a>
+                  </li>
+                 
+
+                  <li>
+                  <form class="d-flex " style="margin-left:70px;">
+                     <input class="form-control me-2" type="search" onkeyup="search_words()" placeholder="Search"  aria-label="Search">
+                  </form>
+                  </li>
+                
+                <li class="nav-item" style="padding-left:100px;"  >
+                <a href="cart.html" class="nav-link">Cart &nbsp;<span><i class="fas fa-shopping-cart fa-lg"><span class="badge badge-primary " style="font-size:12px;position:relative;top:-15px;">
+              ${totalItems()}
+                </span>
+                </i></span></a>      
+               </li>
+
+
+               <li class="nav-item ">
+               <a href="login.html" class="nav-link">Sign In  &nbsp;<span><i class="fas fa-user fa-lg"></i></span></a>
+
+               </li>
+               </ul>
+                
+               <div></div>
+                
+              </div>
+            </div>
+          </nav>
+    </header>
+        
+        `
+    }
+}
+
+customElements.define('my-header', Myheader)
+
+
+
+
+class MyFooter extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML=`
+        
+        <footer id="footer">
+
+
+    <div class="footer2" style="background-color:gray">
+      <div class="container">
+        <div class="row">         
+            <div class="social text-center mt-2"  >
+                <a href="#" style=" color: black;"><i class="fab fa-twitter fa-2x"></i></a>
+                <a href="#" style=" color: black;"><i class="fab fa-facebook-f fa-2x"></i></a> 
+                <a href="#" style=" color: black;"><i class="fab fa-dribbble fa-2x"></i></a>
+                <a href="#" style=" color: black;"><i class="fab fa-flickr fa-2x"></i></a>
+                <a href="#" style=" color: black;"><i class="fab fa-github fa-2x"></i></a>
+            </div>
+          <div class=" panel text-center">
+            <div >
+              <p  class="text-center">Copyright &copy; 2022 E-Shop <br>Designed by Naseer Ahmad</p>
+
+            </div>
+          </div>
+  
+        </div>
+        <!-- /row of panels -->
+      </div>
+    </div>
+  </footer>
+  
+  
+
+        `
+    }
+}
+
+customElements.define('my-footer',MyFooter)
